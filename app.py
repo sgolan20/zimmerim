9e043bd31a702957a993e2aa01e074de19bbc8a8
sgolan20 <34423858+sgolan20@users.noreply.github.com>
2024-12-06 10:13:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -139,4 +139,5 @@ if __name__ == '__main__':
             db.session.commit()
             print("Admin user created successfully")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    app.run()
